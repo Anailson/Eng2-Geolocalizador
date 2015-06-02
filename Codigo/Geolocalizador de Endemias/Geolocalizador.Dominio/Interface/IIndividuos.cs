90218bcdf372009411ca835ba5bf5080dbfd5d26
luiz-fonseca <antoniofonseca1991@gmail.com>
2015-06-02 19:37:33 -0300

    Arquitetura do Projeto

---
+++ Codigo/Geolocalizador de Endemias/Geolocalizador.Dominio/Interface/IIndividuos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Geolocalizador.Dominio.Classes;
+
+
+namespace Geolocalizador.Dominio.Interface
+{
+    interface IIndividuos : IRepositorio<Individuo>
+    {
+        void Cadastrar(Individuo individuo);
+        void Atualizar(Individuo individuo);
+        void Remover(Individuo individuo);
+
+        Individuo ResultadoUnico(int idIndividuo);
+    }
+}
