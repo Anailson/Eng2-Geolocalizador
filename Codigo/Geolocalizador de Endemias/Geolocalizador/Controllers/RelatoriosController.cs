19cb10f4a8cc18b2f5456cdbba4466cc96a595bc
Bárbara Oliveira <barbara.sistemas.ufs@gmail.com>
2015-07-13 20:10:59 -0300

    Adicionado aparte de Relatorio no Projeto e adicionado o relatorio de Visita

---
+++ Codigo/Geolocalizador de Endemias/Geolocalizador/Controllers/RelatoriosController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Reporting.WebForms;
+using Dominio.Interfaces;
+using ReportViewerForMvc;
+
+namespace Geolocalizador.Controllers
+{
+    public class RelatoriosController : Controller
+    {
+        public IVisitas visitas { get; set; }
+
+        // GET: Relatorios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult relatorioVisita()
+        {
+            ReportViewer reporter = new ReportViewer();
+            reporter.ProcessingMode = ProcessingMode.Local;
+
+            reporter.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"Views\Relatorios\RelatorioVisita.rdlc";
+
+            reporter.LocalReport.DataSources
+                .Add(new ReportDataSource("DataSetGeolocalizador", visitas.Listar()));
+
+            reporter.AsyncRendering = false;
+            reporter.SizeToReportContent = true;
+            ViewBag.ReporterViewer = reporter;
+            return View();
+        }
+
+    }
+}
