fa3a036e6ca455278777607d460b9e56d192fb23
Luiz Fonsêca <antoniofonseca1991@gmail.com>
2015-07-27 14:43:34 -0300

    Atualização na autenticação

--- Codigo/Geolocalizador de Endemias/Geolocalizador/App_Start/BundleConfig.cs
+++ Codigo/Geolocalizador de Endemias/Geolocalizador/App_Start/BundleConfig.cs
@@ -9,20 +9,16 @@ namespace Geolocalizador.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Content/js/jquery-{version}.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
+                      "~/Content/js/materialize.js",
+                      "~/Content/js/materialize.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/materialize.css",
+                      "~/Content/css/materialize.min.css"));
         }
     }
 }
